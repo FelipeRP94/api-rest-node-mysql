ec63c451c70670510603f200d2d685d6341f0602
Felipe Ruiz Pinto <felipe.r.p.1994@gmail.com>
2018-02-01 22:04:14 +0100

    Add remote db connection

--- app/common/dbConnection.js
+++ app/common/dbConnection.js
@@ -7,6 +7,13 @@ const dataConnection = {
     database: 'api_rest'
 }
 
-const connection = mysql.createConnection( { ...dataConnection });
+const dataConnectionRemote = {
+    host: '	fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'tm6thls90rwszbws',
+    password: 'qe14a3x595bu86zz',
+    database: 'g3hlx9xe22auxnfr'
+}
+
+const connection = mysql.createConnection( { ...dataConnectionRemote });
 
 module.exports = connection
